e4938e5a815e58427c47e2fbc0b56407814ec90d
Greiza García <greizag@gmail.com>
2024-05-11 04:04:44 +0000

    Agregué: 1.- Cambio de icono de corazón con plus a corazón check al agregar favorito 2.- Si tengo un ítem agregado como favorito y vuelvo a dar click al corazón, este se eliminará y volverá al corazón con el plus

--- src/js/component/PlanetCard.js
+++ src/js/component/PlanetCard.js
@@ -1,30 +1,48 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const PlanetCard = ({ name, id }) => {
-    const {store, actions} = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-    return (
-        <div className="card me-2 col-3 bg-dark">
-            <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg` == "https://starwars-visualguide.com/assets/img/planets/1.jpg" ?
-                "https://i.imgur.com/0zP3R0i.jpeg" :
-                `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-                className="rounded-top card-image-top" />
-            <div className="card-body">
-                <h5 className="card-title mb-4 fw-lighter font-monospace fs-6 fs-md-4" id="planets">{name}</h5>
-                <div className="d-flex justify-content-between">
-                    <p className="text-light">
-                        <i className="fa-solid fa-heart-circle-plus fs-3 text-white-50"
-                            onClick={() => {
-                                actions.addFavorite(id, name);
-                            }}></i>
-                    </p>
-                    <Link to={`/planet-detail/${id}`}>
-                        <button className="btn btn-outline-danger btn-sm">More <i className="fa-solid fa-angles-right"></i></button>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="card me-2 col-3 bg-dark">
+      <img
+        src={
+          `https://starwars-visualguide.com/assets/img/planets/${id}.jpg` ==
+          "https://starwars-visualguide.com/assets/img/planets/1.jpg"
+            ? "https://i.imgur.com/0zP3R0i.jpeg"
+            : `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`
+        }
+        className="rounded-top card-image-top"
+      />
+      <div className="card-body">
+        <h5
+          className="card-title mb-4 fw-lighter font-monospace fs-6 fs-md-4"
+          id="planets"
+        >
+          {name}
+        </h5>
+        <div className="d-flex justify-content-between">
+          <p className="text-light">
+            <i
+              className={`fa-solid fa-heart-circle-${
+                store.favorites.find((favorite) => favorite.name == name)
+                  ? "check text-danger"
+                  : "plus"
+              } fs-3 addFavorite`}
+              onClick={() => {
+                actions.addFavorite(id, name);
+              }}
+            ></i>
+          </p>
+          <Link to={`/planet-detail/${id}`}>
+            <button className="btn btn-outline-danger btn-sm">
+              More <i className="fa-solid fa-angles-right"></i>
+            </button>
+          </Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
--- src/js/component/StarshipCard.js
+++ src/js/component/StarshipCard.js
@@ -1,39 +1,51 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const StarshipCard = ({ name, id }) => {
-    const {store, actions} = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-    return (
-        <div className="card me-2 col-3 bg-dark">
-            <img src={`${id}` == 2 ? "https://i.imgur.com/7Di3y5w.jpeg" :
-                `${id}` == 3 ? "https://i.imgur.com/yw3Gstj.jpeg" :
-                    `${id}` == 17 ? "https://i.imgur.com/HzKyhvj.jpeg" :
-                        `https://starwars-visualguide.com/assets/img/starships/${id}.jpg`}
-                className="rounded-top card-img-top" />
-            <div className="card-body">
-                <h5 className="card-title mb-4 fw-lighter font-monospace fs-6 fs-md-4" id="starships">{name}</h5>
-                <div className="d-flex justify-content-between">
-                    <p className="text-light">
-                        <i className="fa-solid fa-heart-circle-plus fs-3 text-white-50"
-                            onClick={() => {
-                                actions.addFavorite(id, name);
-                            }}></i>
-                    </p>
-                    <Link to={`/starship-detail/${id}`}>
-                        <button className="btn btn-outline-warning btn-sm">More <i className="fa-solid fa-angles-right"></i></button>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="card me-2 col-3 bg-dark">
+      <img
+        src={
+          `${id}` == 2
+            ? "https://i.imgur.com/7Di3y5w.jpeg"
+            : `${id}` == 3
+            ? "https://i.imgur.com/yw3Gstj.jpeg"
+            : `${id}` == 17
+            ? "https://i.imgur.com/HzKyhvj.jpeg"
+            : `https://starwars-visualguide.com/assets/img/starships/${id}.jpg`
+        }
+        className="rounded-top card-img-top"
+      />
+      <div className="card-body">
+        <h5
+          className="card-title mb-4 fw-lighter font-monospace fs-6 fs-md-4"
+          id="starships"
+        >
+          {name}
+        </h5>
+        <div className="d-flex justify-content-between">
+          <p className="text-light">
+            <i
+              className={`fa-solid fa-heart-circle-${
+                store.favorites.find((favorite) => favorite.name == name)
+                  ? "check text-danger"
+                  : "plus"
+              } fs-3 addFavorite`}
+              onClick={() => {
+                actions.addFavorite(id, name);
+              }}
+            ></i>
+          </p>
+          <Link to={`/starship-detail/${id}`}>
+            <button className="btn btn-outline-warning btn-sm">
+              More <i className="fa-solid fa-angles-right"></i>
+            </button>
+          </Link>
         </div>
-    )
-}
-
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+      </div>
+    </div>
+  );
+};
